08adc944b95b3a59ed4ce1bd02458f8db127e017
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-02-19 12:39:21 +0500

    Initial Commit

---
+++ WeeklyFinder/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by Алексей Пархоменко on 19.02.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
+    @IBAction func findDay(_ sender: UIButton) {
+    }
+    
+}
+
